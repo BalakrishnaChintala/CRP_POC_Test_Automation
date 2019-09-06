30aa4be14f73235274ad3a19d0281c9bb23d3bfe
Balakrishna Chintala <53430280+BalakrishnaChintala@users.noreply.github.com>
2019-09-06 14:45:14 +0530

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,16 @@ var angularProtractor = require('gulp-angular-protractor');
             .src(['./*.js']) 
             .pipe(angularProtractor({      
                 'configFile': 'conf.js',
+                'webDriverUpdate': {
+                    args: ['--versions.chrome', '2.28']
+                },
+                'webDriverStart': {
+                    args: ['--versions.chrome', '2.28']
+                },
             }))
+            .on('error', function(e) {
+                console.log(e);
+            })
+            .on('end', callback);
 
 });
